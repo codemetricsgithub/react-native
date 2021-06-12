6a118f66ca8fe86ce52680bd195b2ca486fbae6d
codemetricsgithub <krishna.kumar@codemetrics.in>
2021-06-12 08:39:30 +0530

    development branch

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const App = () => {
           flexDirection: 'row',
         },
       ]}>
-      <View style={{flex: 1, backgroundColor: 'red'}} />
+      <View style={{flex: 1, backgroundColor: 'blue'}} />
       <View style={{flex: 2, backgroundColor: 'darkorange'}} />
       <View style={{flex: 3, backgroundColor: 'green'}} />
     </View>
