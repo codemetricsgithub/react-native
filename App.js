3515639370c1125c9a50d1d278577751aa39b3a7
codemetricsgithub <krishna.kumar@codemetrics.in>
2021-06-12 22:56:50 +0530

    how to get image in react native

--- App.js
+++ App.js
@@ -1,29 +1,19 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {View, Text, Image} from 'react-native';
 
 const App = () => {
   return (
-    <View
-      style={[
-        styles.container,
+    <View>
+      <Text>img here</Text>
+      {/* <Image source={require('./img/2.jpg')} /> */}
 
-        {
-          // Try setting `flexDirection` to `"row"`.
-          flexDirection: 'row',
-        },
-      ]}>
-      <View style={{flex: 1, backgroundColor: 'red'}} />
-      <View style={{flex: 2, backgroundColor: 'darkorange'}} />
-      <View style={{flex: 3, backgroundColor: 'green'}} />
+      {/* <Image
+        source={{uri: 'https://reactjs.org/logo-og.png'}}
+        style={{width: 400, height: 400}}
+
+      /> */}
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-  },
-});
 
 export default App;
---
+++ components/Flexbox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Flexbox = () => {
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: 'red',
+    },
+  });
+
+  return (
+    <View stylesugugu={styles.container}>
+      <Text>hello </Text>
+    </View>
+  );
+};
+
+export default Flexbox;
