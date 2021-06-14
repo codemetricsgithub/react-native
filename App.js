72b5bd27711d713c448c8e6a4f9fa5461fee23d7
codemetricsgithub <krishna.kumar@codemetrics.in>
2021-06-14 16:00:47 +0530

    this is text views

--- App.js
+++ App.js
@@ -1,42 +1,19 @@
 import React from 'react';
-import {useCallback, useRef} from 'react';
-import {
-  StyleSheet,
-  TextInput,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import {View, Text} from 'react-native';
 
 const App = () => {
-  const inputRef = useRef();
-  const clearText = useCallback(() => {
-    inputRef.current.setNativeProps({text: ''});
-  }, []);
-
   return (
-    <View style={styles.container}>
-      <TextInput ref={inputRef} style={styles.input} />
-      <TouchableOpacity onPress={clearText}>
-        <Text>Clear text</Text>
-      </TouchableOpacity>
+    <View
+      style={{
+        flexDirection: 'row',
+        height: 100,
+        padding: 20,
+      }}>
+      <View style={{backgroundColor: 'blue', flex: 0.3}} />
+      <View style={{backgroundColor: 'red', flex: 0.5}} />
+      <Text>Hello World!</Text>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input: {
-    height: 50,
-    width: 200,
-    marginHorizontal: 20,
-    borderWidth: 1,
-    borderColor: '#ccc',
-  },
-});
 
 export default App;
